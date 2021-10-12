a393c50c46d97da7e80a37c2a68402194c4b5478
DevExpressExampleBot <fakeEmail>
2021-10-12 23:42:16 +0300

    autogenerate VB

--- VB/DateFilterDashboardItemSample/Form1.Designer.vb
+++ VB/DateFilterDashboardItemSample/Form1.Designer.vb
@@ -1,61 +1,65 @@
-﻿Namespace DateFilterDashboardItemSample
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace DateFilterDashboardItemSample
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.dashboardViewer1 = New DevExpress.DashboardWin.DashboardViewer(Me.components)
-			DirectCast(Me.dashboardViewer1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' dashboardViewer1
-			' 
-			Me.dashboardViewer1.Appearance.BackColor = System.Drawing.Color.FromArgb((CInt((CByte(242)))), (CInt((CByte(242)))), (CInt((CByte(242)))))
-			Me.dashboardViewer1.Appearance.Options.UseBackColor = True
-			Me.dashboardViewer1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.dashboardViewer1.Location = New System.Drawing.Point(0, 0)
-			Me.dashboardViewer1.Name = "dashboardViewer1"
-			Me.dashboardViewer1.Size = New System.Drawing.Size(810, 452)
-			Me.dashboardViewer1.TabIndex = 0
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(810, 452)
-			Me.Controls.Add(Me.dashboardViewer1)
-			Me.Name = "Form1"
-			Me.Text = "DateFilter Example"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
-			DirectCast(Me.dashboardViewer1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.dashboardViewer1 = New DevExpress.DashboardWin.DashboardViewer(Me.components)
+            CType((Me.dashboardViewer1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' dashboardViewer1
+            ' 
+            Me.dashboardViewer1.Appearance.BackColor = System.Drawing.Color.FromArgb((CInt(((CByte((242)))))), (CInt(((CByte((242)))))), (CInt(((CByte((242)))))))
+            Me.dashboardViewer1.Appearance.Options.UseBackColor = True
+            Me.dashboardViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.dashboardViewer1.Location = New System.Drawing.Point(0, 0)
+            Me.dashboardViewer1.Name = "dashboardViewer1"
+            Me.dashboardViewer1.Size = New System.Drawing.Size(810, 452)
+            Me.dashboardViewer1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(810, 452)
+            Me.Controls.Add(Me.dashboardViewer1)
+            Me.Name = "Form1"
+            Me.Text = "DateFilter Example"
+             ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+'''             this.Load += new System.EventHandler(this.Form1_Load)
+'''  CType((Me.dashboardViewer1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private dashboardViewer1 As DevExpress.DashboardWin.DashboardViewer
-	End Class
+#End Region
+        Private dashboardViewer1 As DevExpress.DashboardWin.DashboardViewer
+    End Class
 End Namespace
-
--- VB/DateFilterDashboardItemSample/Form1.vb
+++ VB/DateFilterDashboardItemSample/Form1.vb
@@ -1,94 +1,84 @@
-﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon
 Imports DevExpress.DashboardWin.Localization
 Imports DevExpress.DataAccess.Excel
 Imports DevExpress.XtraEditors
 Imports System
 
 Namespace DateFilterDashboardItemSample
-	Partial Public Class Form1
-		Inherits XtraForm
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class Form1
+        Inherits XtraForm
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-			Dim excelDataSource As DashboardExcelDataSource = CreateExcelDataSource()
-			Dim dBoard As New Dashboard()
-			dBoard.DataSources.Add(excelDataSource)
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-			dBoard.BeginUpdate()
-			' Create dashboard items.
-			Dim chart As ChartDashboardItem = CreateChart(excelDataSource)
-			dBoard.Items.Add(chart)
-			Dim dateFilterItem As DateFilterDashboardItem = CreateDateFilterItem(excelDataSource)
-			dBoard.Items.Add(dateFilterItem)
-			Dim group As DashboardItemGroup = CreateGroup()
-			dBoard.Groups.Add(group)
-			group.AddRange(dateFilterItem, chart)
-			' Create the layout tree.
-			Dim dateFilterLayoutItem As New DashboardLayoutItem(dateFilterItem, 30)
-			Dim chartLayoutItem As New DashboardLayoutItem(chart, 70)
-			Dim groupLayoutItem As New DashboardLayoutGroup(group, 100)
-			groupLayoutItem.ChildNodes.AddRange(dateFilterLayoutItem, chartLayoutItem)
-            Dim rootGroup As New DashboardLayoutGroup()
-            rootGroup.DashboardItem = Nothing
-            rootGroup.Weight = 100
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs)
+            Dim excelDataSource As DashboardExcelDataSource = Me.CreateExcelDataSource()
+            Dim dBoard As Dashboard = New Dashboard()
+            dBoard.DataSources.Add(excelDataSource)
+            dBoard.BeginUpdate()
+            ' Create dashboard items.
+            Dim chart As ChartDashboardItem = Me.CreateChart(excelDataSource)
+            dBoard.Items.Add(chart)
+            Dim dateFilterItem As DateFilterDashboardItem = Me.CreateDateFilterItem(excelDataSource)
+            dBoard.Items.Add(dateFilterItem)
+            Dim group As DashboardItemGroup = Me.CreateGroup()
+            dBoard.Groups.Add(group)
+            group.AddRange(dateFilterItem, chart)
+            ' Create the layout tree.
+            Dim dateFilterLayoutItem As DashboardLayoutItem = New DashboardLayoutItem(dateFilterItem, 30)
+            Dim chartLayoutItem As DashboardLayoutItem = New DashboardLayoutItem(chart, 70)
+            Dim groupLayoutItem As DashboardLayoutGroup = New DashboardLayoutGroup(group, 100)
+            groupLayoutItem.ChildNodes.AddRange(dateFilterLayoutItem, chartLayoutItem)
+            Dim rootGroup As DashboardLayoutGroup = New DashboardLayoutGroup(Nothing, 100)
             rootGroup.ChildNodes.Add(groupLayoutItem)
-			rootGroup.Orientation = DashboardLayoutGroupOrientation.Vertical
-			dBoard.LayoutRoot = rootGroup
-			dBoard.EndUpdate()
+            rootGroup.Orientation = DashboardLayoutGroupOrientation.Vertical
+            dBoard.LayoutRoot = rootGroup
+            dBoard.EndUpdate()
+            Me.dashboardViewer1.Dashboard = dBoard
+        End Sub
 
-			dashboardViewer1.Dashboard = dBoard
-		End Sub
+        Private Function CreateGroup() As DashboardItemGroup
+            Dim group As DashboardItemGroup = New DashboardItemGroup()
+            group.Name = "Sales by Date"
+            Return group
+        End Function
 
-		Private Function CreateGroup() As DashboardItemGroup
-			Dim group As New DashboardItemGroup()
-			group.Name = "Sales by Date"
-			Return group
-		End Function
+        Private Function CreateDateFilterItem(ByVal excelDataSource As DashboardExcelDataSource) As DateFilterDashboardItem
+            Dim dateFilter As DateFilterDashboardItem = New DateFilterDashboardItem()
+            dateFilter.Name = String.Empty
+            dateFilter.ShowCaption = False
+            dateFilter.DataSource = excelDataSource
+            dateFilter.Dimension = New Dimension("orderDateId", "OrderDate", DateTimeGroupInterval.DayMonthYear)
+            dateFilter.Dimension.DateTimeFormat.DateTimeFormat = DateTimeFormat.[Short]
+            dateFilter.ArrangementMode = DateFilterArrangementMode.Vertical
+            dateFilter.FilterType = DateFilterType.Between
+            dateFilter.DatePickerLocation = DatePickerLocation.Far
+            dateFilter.DateTimePeriods.AddRange(DateTimePeriod.CreateLastYear(), DateTimePeriod.CreateNextDays("Next 7 Days", 7), New DateTimePeriod With {.Name = DashboardWinLocalizer.GetString(DashboardWinStringId.PeriodMonthToDate), .Start = New FlowDateTimePeriodLimit With {.Interval = DateTimeInterval.Month, .Offset = 0}, .[End] = New FlowDateTimePeriodLimit With {.Interval = DateTimeInterval.Day, .Offset = 1}}, New DateTimePeriod With {.Name = "Jul-18-2018 - Jan-18-2019", .Start = New FixedDateTimePeriodLimit With {.[Date] = New System.DateTime(2018, 7, 18)}, .[End] = New FixedDateTimePeriodLimit With {.[Date] = New System.DateTime(2019, 1, 18)}})
+            Return dateFilter
+        End Function
 
-		Private Function CreateDateFilterItem(ByVal excelDataSource As DashboardExcelDataSource) As DateFilterDashboardItem
-			Dim dateFilter As New DateFilterDashboardItem()
-			dateFilter.Name = String.Empty
-			dateFilter.ShowCaption = False
-			dateFilter.DataSource = excelDataSource
-			dateFilter.Dimension = New Dimension("orderDateId", "OrderDate", DateTimeGroupInterval.DayMonthYear)
-			dateFilter.Dimension.DateTimeFormat.DateTimeFormat = DateTimeFormat.Short
-			dateFilter.ArrangementMode = DateFilterArrangementMode.Vertical
-			dateFilter.FilterType = DateFilterType.Between
-			dateFilter.DatePickerLocation = DatePickerLocation.Far
-			dateFilter.DateTimePeriods.AddRange(DateTimePeriod.CreateLastYear(), DateTimePeriod.CreateNextDays("Next 7 Days", 7), New DateTimePeriod With {
-				.Name = DashboardWinLocalizer.GetString(DashboardWinStringId.PeriodMonthToDate), .Start = New FlowDateTimePeriodLimit With {.Interval = DateTimeInterval.Month, .Offset = 0},
-				.End = New FlowDateTimePeriodLimit With {.Interval = DateTimeInterval.Day, .Offset = 1}
-			},
-			New DateTimePeriod With {
-				.Name = "Jul-18-2018 - Jan-18-2019", .Start = New FixedDateTimePeriodLimit With {.Date = New Date(2018, 7, 18)},
-				.End = New FixedDateTimePeriodLimit With {.Date = New Date(2019, 1, 18)}
-			})
-			Return dateFilter
-		End Function
+        Private Function CreateChart(ByVal excelDataSource As DashboardExcelDataSource) As ChartDashboardItem
+            Dim chart As ChartDashboardItem = New ChartDashboardItem()
+            chart.Name = String.Empty
+            chart.ShowCaption = False
+            chart.DataSource = excelDataSource
+            chart.Arguments.Add(New Dimension("OrderDate", DateTimeGroupInterval.DayMonthYear))
+            chart.Panes.Add(New ChartPane())
+            Dim salesAmountSeries As SimpleSeries = New SimpleSeries(SimpleSeriesType.Line)
+            salesAmountSeries.Value = New Measure("Extended Price")
+            chart.Panes(CInt((0))).Series.Add(salesAmountSeries)
+            Return chart
+        End Function
 
-		Private Function CreateChart(ByVal excelDataSource As DashboardExcelDataSource) As ChartDashboardItem
-			Dim chart As New ChartDashboardItem()
-			chart.Name = String.Empty
-			chart.ShowCaption = False
-			chart.DataSource = excelDataSource
-			chart.Arguments.Add(New Dimension("OrderDate", DateTimeGroupInterval.DayMonthYear))
-			chart.Panes.Add(New ChartPane())
-			Dim salesAmountSeries As New SimpleSeries(SimpleSeriesType.Line)
-			salesAmountSeries.Value = New Measure("Extended Price")
-			chart.Panes(0).Series.Add(salesAmountSeries)
-			Return chart
-		End Function
-
-		Private Function CreateExcelDataSource() As DashboardExcelDataSource
-			Dim excelDataSource As New DashboardExcelDataSource()
-			excelDataSource.FileName = "SalesPerson.xlsx"
-			Dim worksheetSettings As New ExcelWorksheetSettings("Data")
-			excelDataSource.SourceOptions = New ExcelSourceOptions(worksheetSettings)
-			excelDataSource.Fill()
-			Return excelDataSource
-		End Function
-	End Class
+        Private Function CreateExcelDataSource() As DashboardExcelDataSource
+            Dim excelDataSource As DashboardExcelDataSource = New DashboardExcelDataSource()
+            excelDataSource.FileName = "SalesPerson.xlsx"
+            Dim worksheetSettings As ExcelWorksheetSettings = New ExcelWorksheetSettings("Data")
+            excelDataSource.SourceOptions = New ExcelSourceOptions(worksheetSettings)
+            excelDataSource.Fill()
+            Return excelDataSource
+        End Function
+    End Class
 End Namespace
